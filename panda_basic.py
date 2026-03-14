48b3cde395ca49d33026f4df6df850bd6da4cb4e
siddharth1728 <siddharthreddy1728@gmail.com>
2026-03-14 20:51:31 +0530

    Hi

--- panda_basic.py
+++ panda_basic.py
@@ -1,10 +1,17 @@
 import pandas as pd
-df = pd.read_excel("titanic.csv.xlsx")
-a = df.describe()
-print(df.head(11)) #it list 5 records
 
+df = pd.read_excel("titanic.csv.xlsx")
+
+m = df.groupby("Age").mean(numeric_only=True)
+print(m)
+
+# print(m)
 # print(df)   
 # print(type(df))
 # print(df.dtypes)
+# print(df.columns)
+# print(df.axes)
+# print(df.Sex)
+# print()
 # print(df.shape)
 # print(a)
